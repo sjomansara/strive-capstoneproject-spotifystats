e8242541b753aca695a02572b889282fbc7986de
sjomansara <sara.sjoman@hotmail.com>
2021-09-28 19:54:55 +0200

    created MyRectangle

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import NavbarHome from './components/NavbarHome';
 import MyJumbotron from './components/MyJumbotron';
 import MyFooter from './components/MyFooter';
+import MyRectangle from './components/MyRectangle';
+import { Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <NavbarHome />
+      <Row>
       <MyJumbotron />
+      <MyRectangle />
+      </Row>
       <MyFooter />
     </div>
   );
