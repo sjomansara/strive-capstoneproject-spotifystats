0d07315f4d0dd06ac9809c6354de95602aa009e3
sjomansara <sara.sjoman@hotmail.com>
2021-09-28 20:22:19 +0200

    Update MyFooter.jsx

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MyFooter = () => (
     <div className="footer">
-      <p className="mt-2 mr-4">&copy; 2021</p>
+      <p className="mt-2 mr-1">&copy; 2021</p>
     </div>
   );
   
