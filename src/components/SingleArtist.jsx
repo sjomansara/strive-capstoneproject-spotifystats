9e982f34c1330a462a4d8b5ebcf71f621976230a
sjomansara <sara.sjoman@hotmail.com>
2021-10-01 15:33:46 +0200

    top artists done

--- src/components/SingleArtist.jsx
+++ src/components/SingleArtist.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const SingleArtist = (props) => {
     return (
-            <Card id="albumCard" className="" style={{ width: '13.2rem' }}>
+            <Card id="albumCard" className="ml-3 mb-3" style={{ width: '13.25rem' }}>
             <Link to="/details" id="linkHover"><Card.Img className="rounded-0" variant="top" src={props.img} /></Link>
             <Card.Body>
             <Link to="/details" id="linkHover"><Card.Title key={props.id} id="trackName" className="text-muted">{props.number}. {props.artist}</Card.Title></Link>
